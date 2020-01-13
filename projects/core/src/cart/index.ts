ea632861c28857ca086b165628a6e10e23dd0c0e
Stan <dunqan@w9.pl>
2020-01-13 11:50:51 +0100

    Update index.ts

--- projects/core/src/cart/index.ts
+++ projects/core/src/cart/index.ts
@@ -1,6 +1,7 @@
 export * from './cart.module';
 export * from './connectors/index';
 export * from './facade/index';
+// sobie commit
 export * from './store/cart-state';
 export * from './store/index';
 export * from './store/selectors/index';
